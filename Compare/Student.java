c787b753a15e84679933003fe1bef5b252c5adcb
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2026-02-26 22:42:41 -0300

    Finish Student

--- Compare/Student.java
+++ Compare/Student.java
@@ -1,4 +1,7 @@
 package Compare;
+
+import java.util.Objects;
+
 public class Student implements Comparable<Student>{
     private String name;
     private Integer registration;
@@ -28,6 +31,18 @@ public class Student implements Comparable<Student>{
 
     public int compareTo(Student o) {
         return -registration.compareTo(o.getRegistration());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) && Objects.equals(registration, student.registration);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, registration);
     }
 
 }
