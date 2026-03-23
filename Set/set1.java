51be8357a50ce0f9cb608cf5e3871692906989ac
João Luis Damasceno <130399587+JoaoLuisXD@users.noreply.github.com>
2026-03-22 23:19:47 -0300

    Set Structure

--- Set/set1.java
+++ Set/set1.java
@@ -1,11 +1,10 @@
 package Set;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
-import java.util.TreeSet;
+
 
 public class set1 {
     public static void main(String[] args) {
